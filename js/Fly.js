5044d10cac1161ed9415d93d4acf3011cf72358b
pao <751189277@qq.com>
2016-12-02 00:19:55 +0800

    add different event for different device

--- js/Fly.js
+++ js/Fly.js
@@ -2,7 +2,7 @@
  * @Author: pao
  * @Date:   2016-11-28 21:03:25
  * @Last Modified by:   pao
- * @Last Modified time: 2016-11-29 20:46:40
+ * @Last Modified time: 2016-12-02 00:08:03
  */
 
 'use strict';
@@ -29,6 +29,30 @@ Fly.loadImg = function(callback) {
 Fly.toRadian = function(angle) {
     return angle * Math.PI / 180;
 };
-Fly.tap = function() {
-
-    } //移动端的点击函数
+Fly.tap = function(dom, callback) {
+    /*1.不能滑动过
+     * 2.end与start时间间隔一般在150ms内*/
+    var isMove = false; //标记是否滑动过
+    var startTime = 0; //记录开始触摸的时间
+    var preventDrag = function(e) {
+        e.preventDefault();
+    }
+    dom.addEventListener("touchstart", function(e) {
+        /*记录开始触摸时间--以毫秒做为单位*/
+        startTime = Date.now();
+    });
+    dom.addEventListener("touchmove", function(e) {
+        isMove = true;
+        document.body.addEventListener('touchmove', preventDrag);
+    });
+    dom.addEventListener("touchend", function(e) {
+        if (isMove == false && Date.now() - startTime <= 250) {
+            callback && callback.call(this, e);
+        }
+        if (isMove) {
+            document.body.removeEventListener('touchmove', preventDrag);
+        }
+        /*重置标记是否滑动*/
+        isMove = false;
+    });
+};
